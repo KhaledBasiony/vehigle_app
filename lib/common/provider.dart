143ba19ac5131cf9773bacb7cbc33720fba043cd
Khaled Basiony <basiony.khaled@outlook.com>
2024-06-22 07:16:30 +0300

    remove gyro provider

--- lib/common/provider.dart
+++ lib/common/provider.dart
@@ -2,28 +2,6 @@ import 'dart:math';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final gyroEventProvider = StateNotifierProvider<GyroNotifier, GyroData>((ref) => GyroNotifier());
-
-class GyroData {
-  GyroData({
-    required this.x,
-    required this.y,
-    required this.z,
-  });
-
-  final double x;
-  final double y;
-  final double z;
-}
-
-class GyroNotifier extends StateNotifier<GyroData> {
-  GyroNotifier() : super(GyroData(x: 0, y: 0, z: 0));
-
-  void changeData(double x, double y, double z) {
-    state = GyroData(x: x, y: y, z: z);
-  }
-}
-
 class LogsNotifier extends StateNotifier<List<String>> {
   LogsNotifier() : super([]);
 

1bcee5dedf4468a98245680d119318cf6c0c298f
Khaled Basiony <basiony.khaled@outlook.com>
2024-06-22 07:07:41 +0300

    add code for mock server to up/down send dummy data

--- lib/models/simulator.dart
+++ lib/models/simulator.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
 class MockServer {
@@ -11,19 +13,57 @@ class MockServer {
   factory MockServer.singleton() => _instance ??= MockServer._();
 
   Future<void> up() async {
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      _sendData();
+    });
     if (_socket != null) {
-      print('Server already up');
+      print('Mock Server already up');
       return;
     }
-    _socket = await ServerSocket.bind(ip, port);
+    _socket = await ServerSocket.bind(ip, port, shared: true);
+
+    _socket?.listen(
+      (event) async {
+        clientSocket = event;
+      },
+      cancelOnError: true,
+    );
   }
 
   Future<void> down() async {
+    _timer.cancel();
+    clientSocket?.destroy();
     await _socket?.close();
+    clientSocket = null;
     _socket = null;
   }
 
+  void _sendData() {
+    clientSocket?.add(
+      utf8.encode(jsonEncode({
+        'CF': 0,
+        'CB': 0,
+        'LF': 0,
+        'LB': 0,
+        'LC': 0,
+        'RF': 0,
+        'RB': 0,
+        'RC': 0,
+        'ENC': 0,
+        'CMPS': 0,
+        'PHS': 0,
+        'ALG': 0,
+        'PRM_A': 0,
+        'PRM_B': 0,
+        'PRM_C': 0,
+        'PRM_D': 0,
+      })),
+    );
+  }
+
   ServerSocket? _socket;
+  Socket? clientSocket;
+  late Timer _timer;
   final String ip;
   final int port;
 }

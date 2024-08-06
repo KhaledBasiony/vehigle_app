2b24e09e4e19c19c8a32bac47b30c8ca2968aa1b
Khaled Basiony <basiony.khaled@outlook.com>
2024-08-06 16:15:13 +0300

    refresh connection when app is inactive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mobile_car_sim/common/db.dart';
 import 'package:mobile_car_sim/common/provider.dart';
